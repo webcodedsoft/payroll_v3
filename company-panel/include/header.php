f6fe892148885001d6557c585a1cd568f42a8f46
Adedeji Sikiru <adedejisikiruadesola@gmail.com>
2020-10-17 09:19:08 +0100

    Update header.php

--- company-panel/include/header.php
+++ company-panel/include/header.php
@@ -7,11 +7,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
-    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
-    <meta name="author" content="PIXINVENT">
-    <title>Dashboard eCommerce - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template
-        + Bitcoin Dashboard</title>
+    <meta name="description" content="Payroll Dashboard">
+    <meta name="keywords" content="Payroll Dashboard">
+    <meta name="author" content="Webcoded">
+    <title>Payroll Dashboard</title>
     <link rel="apple-touch-icon" href="<?php echo $result["Web_Url"]; ?>public/app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo $result["Web_Url"]; ?>public/app-assets/images/ico/favicon.ico">
     <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700" rel="stylesheet">
@@ -69,8 +68,8 @@
 
 <!-- <?php
 
-if (!strpos($server, 'setup')) {
-    include("../include/sidebar.php");
-}
+        if (!strpos($server, 'setup')) {
+            include("../include/sidebar.php");
+        }
 
-?> -->
+        ?> -->
